d530b3a71b42aa0087947004314205c584d050d5
Iyzyman <101888183+Iyzyman@users.noreply.github.com>
2025-01-20 22:58:42 +0800

    feat: edit images path

--- dashboardUI/src/data/members.js
+++ dashboardUI/src/data/members.js
@@ -3,25 +3,25 @@ export const members = [
     name: 'Dmitrii Ustiugov',
     affiliation: 'Nanyang Technological University, Singapore',
     link: 'https://ustiugov.github.io/',
-    photo: '/vhive-serverless/LLM-Benchmarking/static/avatars/dmitrii.jpeg',
+    photo: '/LLM-Benchmarking/static/avatars/dmitrii.jpeg',
   },
   {
     name: 'Yixin Zhang',
     affiliation: 'Nanyang Technological University, Singapore',
     link: 'https://jasonzhang517.github.io/',
-    photo: '/vhive-serverless/LLM-Benchmarking/static/avatars/yixin.jpg',
+    photo: '/LLM-Benchmarking/static/avatars/yixin.jpg',
   },
   {
     name: 'Ian Yeoh',
     affiliation: 'Nanyang Technological University, Singapore',
     link: 'https://www.linkedin.com/in/ian-yeoh-027786182/',
-    photo: '/vhive-serverless/LLM-Benchmarking/static/avatars/ian.jpg',
+    photo: '/LLM-Benchmarking/static/avatars/ian.jpg',
   },
   {
     name: 'Kavita Sriram',
     affiliation: 'Nanyang Technological University, Singapore',
     link: 'https://www.linkedin.com/in/kavita-sriram/',
-    photo: '/vhive-serverless/LLM-Benchmarking/static/avatars/ian.jpg',
+    photo: '/LLM-Benchmarking/static/avatars/ian.jpg',
   }
 ]
 
@@ -30,12 +30,12 @@ export const members = [
 //     name: 'Mária Ďuračková',
 //     affiliation: 'University of Edinburgh, United Kingdom',
 //     link: 'https://www.linkedin.com/in/maria-durackova-34a4631b6/',
-//     photo: '/vhive-serverless/LLM-Benchmarking/static/avatars/maria.jpg',
+//     photo: '/LLM-Benchmarking/static/avatars/maria.jpg',
 //   },
 //   {
 //     name: ' Theodor Amariucai ',
 //     affiliation: 'ETH Zurich, Switzerland',
 //     link: 'https://github.com/amariucaitheodor',
-//     photo: '/vhive-serverless/LLM-Benchmarking/static/avatars/theodor.jpeg',
+//     photo: '/LLM-Benchmarking/static/avatars/theodor.jpeg',
 //   },
 // ]
