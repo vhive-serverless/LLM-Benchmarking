5159c181548bc861838cca0870c9d271afcb657b
kavi-99 <kavisathu96@gmail.com>
2025-02-11 02:44:48 +0800

    add vllm provider

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from providers import (
     Hyperbolic,
     Azure,
     AWSBedrock,
+    vLLM
 )
 from utils.prompt_generator import get_prompt
 
@@ -54,6 +55,7 @@ def get_available_providers():
         "Groq": GroqProvider(),
         "Azure": Azure(),
         "AWSBedrock": AWSBedrock(),
+        "vLLM": vLLM()
     }
 
     return available_providers
